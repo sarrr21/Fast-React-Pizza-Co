e6731e13c5c4bd72bbd6adbb3d88655f83307cf7
sarrr21 <gs9425188@gmail.com>
2024-04-14 22:16:50 +0300

    update

--- pizza.co/src/App.jsx
+++ pizza.co/src/App.jsx
@@ -1,40 +1,49 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./ui/Home";
-import Error from "./ui/Error";
-import Menu, { loader as menuLoader } from "./featuer/menu/Menu";
-import Cart from "./featuer/cart/Cart";
-import CreateOrder from "./featuer/order/CreateOrder";
-import Order from "./featuer/order/Order";
-import AppLayout from "./ui/AppLayout";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     errorElement: <Error />,
-    children:[
+
+    children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
+      { path: '/cart', element: <Cart /> },
       {
-        path: "/cart",
-        element: <Cart />,
+        path: '/order/new',
+        element: <CreateOrder />,
+        action: createOrderAction,
       },
-      {path: "/order/new", element: <CreateOrder />,},
-      {path: "/order/:orderId", element: <Order />,}
-    
-    ]
-  }
-]); 
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
 
-export default function App() {
+function App() {
   return <RouterProvider router={router} />;
-  
 }
+
+export default App;
